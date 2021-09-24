ea28b285b92d95407aed158e50062b00a83f212a
jayfullstack <jayfullstack@yahoo.com>
2021-09-24 19:11:35 -0400

    Fixed drag issue, project complete

--- javascript/main.js
+++ javascript/main.js
@@ -22,6 +22,7 @@ let listArrays = [];
 
 // Drag Functionality
 let draggedItem;
+let dragging = false;
 let currentColumn;
 
 // Get Arrays from localStorage if available, set default values if not
@@ -113,10 +114,14 @@ function updateDOM() {
 function updateItem(id, column){
   const selectedArray = listArrays[column];
   const selectedColumnEl = listColumns[column].children;
-  if (!selectedColumnEl[id].textContent){
-    delete selectedArray[id];
-  }
+  if(!dragging){
+    if (!selectedColumnEl[id].textContent){
+      delete selectedArray[id];
+    }else{
+      selectedArray[id] = selectedColumnEl[id].textContent;
+    }
   updateDOM();
+  }
 }
 
 // Add to Column List,
@@ -168,6 +173,7 @@ function rebuildArrays(){
 // when Item Stars Dragging
 function drag(e) {
   draggedItem = e.target;
+  dragging = true;
 }
 
 // Column Allows for Item to Drop
@@ -191,6 +197,8 @@ function drop(e){
   // Add Item to Column
   const parent = listColumns[currentColumn];
   parent.appendChild(draggedItem);
+  // Dragging Complete
+  dragging = true;
   rebuildArrays();
 }
 
